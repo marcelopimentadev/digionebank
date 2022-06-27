bda8ab4050bf10d4040069a9e658decd9ab675a9
marcelopimentadev <marcelobarpi@gmail.com>
2022-06-27 20:40:43 -0300

    update tp talk about class members

--- src/main/kotlin/one/digitalinnovation/digionebanck/Pessoas.kt
+++ src/main/kotlin/one/digitalinnovation/digionebanck/Pessoas.kt
@@ -4,12 +4,17 @@ class Pessoas {
     var nome: String = "Marcelo Pimenta"
     var cpf: String = "123.123.123.12"
     private set
+
+    constructor()
+
+    fun pessoaInfo() = "$nome e $cpf"
 }
 
 fun main() {
     val nomepessoa = Pessoas()
 
-    println(nomepessoa)
-    println(nomepessoa.nome)
-    println(nomepessoa.cpf)
+    println(nomepessoa.pessoaInfo())
+    //println(nomepessoa)
+    //println(nomepessoa.nome)
+    //println(nomepessoa.cpf)
 }
