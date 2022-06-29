de9ad29941c1d88979fc3c2ff2dde6a1e526b8e6
marcelopimentadev <marcelobarpi@gmail.com>
2022-06-28 21:39:03 -0300

    created enum client type and test to it

---
+++ src/main/kotlin/one/digitalinnovation/digionebanck/ClienteTipo.kt
@@ -0,0 +1,7 @@
+package one.digitalinnovation.digionebanck
+
+enum class ClienteTipo(val descricao: String) {
+    PF("Pessoa Fisica"),
+    PJ("Pessoa Juridica");
+
+}
---
+++ src/main/kotlin/one/digitalinnovation/digionebanck/teste/TesteClienteTipoEnum.kt
@@ -0,0 +1,17 @@
+package one.digitalinnovation.digionebanck.teste
+
+import one.digitalinnovation.digionebanck.ClienteTipo
+
+fun main() {
+    ClienteTipo.values().forEach { elemento ->
+        println("${elemento.name} - ${elemento.descricao}")
+    }
+    ClienteTipo.values().forEach {
+        println("${it.name} -${it.descricao}")
+    }
+    val pf = ClienteTipo.PF
+    println(">>${pf.name} - ${pf.descricao}")
+
+    val pj = ClienteTipo.PJ
+    println(">>${pj.name} - ${pj.descricao}")
+}
